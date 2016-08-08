611a6ad144a5a1174c8958d20f56f35f9bdccd39
Jorge Aparicio <japaricious@gmail.com>
2016-08-08 16:27:58 -0500

    add weak memcpy, memmove and memset

--- src/arm.rs
+++ src/arm.rs
@@ -1,60 +1,54 @@
-extern "C" {
-    fn memcpy(dest: *mut u8, src: *const u8, n: usize) -> *mut u8;
-    fn memmove(dest: *mut u8, src: *const u8, n: usize) -> *mut u8;
-    fn memset(dest: *mut u8, c: i32, n: usize) -> *mut u8;
-}
-
 // FIXME: The `*4` and `*8` variants should be defined as aliases.
 
 #[no_mangle]
 pub unsafe extern "aapcs" fn __aeabi_memcpy(dest: *mut u8, src: *const u8, n: usize) {
-    memcpy(dest, src, n);
+    ::libc::memcpy(dest, src, n);
 }
 #[no_mangle]
 pub unsafe extern "aapcs" fn __aeabi_memcpy4(dest: *mut u8, src: *const u8, n: usize) {
-    memcpy(dest, src, n);
+    ::libc::memcpy(dest, src, n);
 }
 #[no_mangle]
 pub unsafe extern "aapcs" fn __aeabi_memcpy8(dest: *mut u8, src: *const u8, n: usize) {
-    memcpy(dest, src, n);
+    ::libc::memcpy(dest, src, n);
 }
 
 #[no_mangle]
 pub unsafe extern "aapcs" fn __aeabi_memmove(dest: *mut u8, src: *const u8, n: usize) {
-    memmove(dest, src, n);
+    ::libc::memmove(dest, src, n);
 }
 #[no_mangle]
 pub unsafe extern "aapcs" fn __aeabi_memmove4(dest: *mut u8, src: *const u8, n: usize) {
-    memmove(dest, src, n);
+    ::libc::memmove(dest, src, n);
 }
 #[no_mangle]
 pub unsafe extern "aapcs" fn __aeabi_memmove8(dest: *mut u8, src: *const u8, n: usize) {
-    memmove(dest, src, n);
+    ::libc::memmove(dest, src, n);
 }
 
 // Note the different argument order
 #[no_mangle]
 pub unsafe extern "aapcs" fn __aeabi_memset(dest: *mut u8, n: usize, c: i32) {
-    memset(dest, c, n);
+    ::libc::memset(dest, c, n);
 }
 #[no_mangle]
 pub unsafe extern "aapcs" fn __aeabi_memset4(dest: *mut u8, n: usize, c: i32) {
-    memset(dest, c, n);
+    ::libc::memset(dest, c, n);
 }
 #[no_mangle]
 pub unsafe extern "aapcs" fn __aeabi_memset8(dest: *mut u8, n: usize, c: i32) {
-    memset(dest, c, n);
+    ::libc::memset(dest, c, n);
 }
 
 #[no_mangle]
 pub unsafe extern "aapcs" fn __aeabi_memclr(dest: *mut u8, n: usize) {
-    memset(dest, 0, n);
+    ::libc::memset(dest, 0, n);
 }
 #[no_mangle]
 pub unsafe extern "aapcs" fn __aeabi_memclr4(dest: *mut u8, n: usize) {
-    memset(dest, 0, n);
+    ::libc::memset(dest, 0, n);
 }
 #[no_mangle]
 pub unsafe extern "aapcs" fn __aeabi_memclr8(dest: *mut u8, n: usize) {
-    memset(dest, 0, n);
+    ::libc::memset(dest, 0, n);
 }
