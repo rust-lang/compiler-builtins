c1e330e883ded52cd70225f4b91e77a800fed53b
Tamir Duberstein <tamird@gmail.com>
2017-10-06 18:52:54 -0400

    Remove redundant rerun-if-changed directive
    The docs at doc.crates.io/build-script.html state:
    
    > Note that if the build script itself (or one of its dependencies)
    > changes, then it's rebuilt and rerun unconditionally, so
    > cargo:rerun-if-changed=build.rs is almost always redundant (unless you
    > want to ignore changes in all other files except for build.rs).
    
    So remove this redundant thing.

--- build.rs
+++ build.rs
@@ -3,8 +3,6 @@
 use std::env;
 
 fn main() {
-    println!("cargo:rerun-if-changed=build.rs");
-
     let target = env::var("TARGET").unwrap();
 
     // Emscripten's runtime includes all the builtins
