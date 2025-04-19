80f5bd7cfca58ce6660ca83fd04c25f953f5f438
Trevor Gross <tmgross@umich.edu>
2025-04-19 09:40:40 +0000

    testing

--- ci/run.sh
+++ ci/run.sh
@@ -20,28 +20,75 @@ if [ "${USING_CONTAINER_RUSTC:-}" = 1 ]; then
         rustup target add "$target"
 fi
 
-# Test our implementation
-if [ "${BUILD_ONLY:-}" = "1" ]; then
-    echo "nothing to do for no_std"
-else
-    run="cargo test --package builtins-test --no-fail-fast --target $target"
-    $run
-    $run --release
-    $run --features c
-    $run --features c --release
-    $run --features no-asm
-    $run --features no-asm --release
-    $run --features no-f16-f128
-    $run --features no-f16-f128 --release
-    $run --benches
-    $run --benches --release
-fi
+# # Test our implementation
+# if [ "${BUILD_ONLY:-}" = "1" ]; then
+#     echo "nothing to do for no_std"
+# else
+#     run="cargo test --package builtins-test --no-fail-fast --target $target"
+#     $run
+#     $run --release
+#     $run --features c
+#     $run --features c --release
+#     $run --features no-asm
+#     $run --features no-asm --release
+#     $run --features no-f16-f128
+#     $run --features no-f16-f128 --release
+#     $run --benches
+#     $run --benches --release
+# fi
 
-if [ "${TEST_VERBATIM:-}" = "1" ]; then
-    verb_path=$(cmd.exe //C echo \\\\?\\%cd%\\builtins-test\\target2)
-    cargo build --package builtins-test \
-        --target "$target" --target-dir "$verb_path" --features c
-fi
+# if [ "${TEST_VERBATIM:-}" = "1" ]; then
+#     verb_path=$(cmd.exe //C echo \\\\?\\%cd%\\builtins-test\\target2)
+#     cargo build --package builtins-test \
+#         --target "$target" --target-dir "$verb_path" --features c
+# fi
+
+
+## REMOVE
+declare -a rlib_paths
+
+# Set the `rlib_paths` global array to a list of all compiler-builtins rlibs
+update_rlib_paths() {
+    if [ -d /builtins-target ]; then
+        rlib_paths=( /builtins-target/"${target}"/debug/deps/libcompiler_builtins-*.rlib )
+    else
+        rlib_paths=( target/"${target}"/debug/deps/libcompiler_builtins-*.rlib )
+    fi
+}
+
+
+update_rlib_paths
+for rlib in "${rlib_paths[@]}"; do
+    set +x
+    echo "================================================================"
+    echo "checking $rlib for references to core"
+    echo "================================================================"
+    set -x
+
+    tmpdir="${CARGO_TARGET_DIR:-target}/tmp"
+    test -d "$tmpdir" || mkdir "$tmpdir"
+    defined="$tmpdir/defined_symbols.txt"
+    undefined="$tmpdir/defined_symbols.txt"
+
+    $NM --quiet -U "$rlib" | grep 'T _ZN4core' | awk '{print $3}' | sort | uniq > "$defined"
+    $NM --quiet -u "$rlib" | grep 'U _ZN4core' | awk '{print $2}' | sort | uniq > "$undefined"
+    grep_has_results=0
+    grep -v -F -x -f "$defined" "$undefined" && grep_has_results=1
+
+    wc -l $defined
+    wc -l $undefined
+
+    if [ "$target" = "powerpc64-unknown-linux-gnu" ]; then
+        echo "FIXME: powerpc64 fails these tests"
+    elif [ "$grep_has_results" != 0 ]; then
+        echo "error: found unexpected references to core"
+        exit 1
+    else
+        echo "success; no references to core found"
+    fi
+done
+## END REMOVE
+
 
 # Run a command for each `compiler_builtins` rlib file
 for_each_rlib() {
@@ -128,45 +175,3 @@ for_each_rlib nm -A
 for_each_rlib "${symcheck[@]}" -- check-core-syms
 
 
-declare -a rlib_paths
-
-# Set the `rlib_paths` global array to a list of all compiler-builtins rlibs
-update_rlib_paths() {
-    if [ -d /builtins-target ]; then
-        rlib_paths=( /builtins-target/"${target}"/debug/deps/libcompiler_builtins-*.rlib )
-    else
-        rlib_paths=( target/"${target}"/debug/deps/libcompiler_builtins-*.rlib )
-    fi
-}
-
-
-update_rlib_paths
-for rlib in "${rlib_paths[@]}"; do
-    set +x
-    echo "================================================================"
-    echo "checking $rlib for references to core"
-    echo "================================================================"
-    set -x
-
-    tmpdir="${CARGO_TARGET_DIR:-target}/tmp"
-    test -d "$tmpdir" || mkdir "$tmpdir"
-    defined="$tmpdir/defined_symbols.txt"
-    undefined="$tmpdir/defined_symbols.txt"
-
-    $NM --quiet -U "$rlib" | grep 'T _ZN4core' | awk '{print $3}' | sort | uniq > "$defined"
-    $NM --quiet -u "$rlib" | grep 'U _ZN4core' | awk '{print $2}' | sort | uniq > "$undefined"
-    grep_has_results=0
-    grep -v -F -x -f "$defined" "$undefined" && grep_has_results=1
-
-    wc -l $defined
-    wc -l $undefined
-
-    if [ "$target" = "powerpc64-unknown-linux-gnu" ]; then
-        echo "FIXME: powerpc64 fails these tests"
-    elif [ "$grep_has_results" != 0 ]; then
-        echo "error: found unexpected references to core"
-        exit 1
-    else
-        echo "success; no references to core found"
-    fi
-done
