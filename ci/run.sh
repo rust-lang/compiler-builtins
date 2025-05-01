be88a3156ead3ecd59f5308908805d7fbe0a3a09
Trevor Gross <tmgross@umich.edu>
2025-05-01 21:30:53 +0000

    simplify CI more, exclude tests of compiler-builtins

--- ci/run.sh
+++ ci/run.sh
@@ -25,152 +25,152 @@ if [ "${USING_CONTAINER_RUSTC:-}" = 1 ]; then
         rustup target add "$target"
 fi
 
-# Test our implementation
-if [ "${BUILD_ONLY:-}" = "1" ]; then
-    echo "no tests to run for build-only targets"
-else
-    test_builtins=(cargo test --package builtins-test --no-fail-fast --target "$target")
-    "${test_builtins[@]}"
-    "${test_builtins[@]}" --release
-    "${test_builtins[@]}" --features c
-    "${test_builtins[@]}" --features c --release
-    "${test_builtins[@]}" --features no-asm
-    "${test_builtins[@]}" --features no-asm --release
-    "${test_builtins[@]}" --features no-f16-f128
-    "${test_builtins[@]}" --features no-f16-f128 --release
-    "${test_builtins[@]}" --benches
-    "${test_builtins[@]}" --benches --release
+# # Test our implementation
+# if [ "${BUILD_ONLY:-}" = "1" ]; then
+#     echo "no tests to run for build-only targets"
+# else
+#     test_builtins=(cargo test --package builtins-test --no-fail-fast --target "$target")
+#     "${test_builtins[@]}"
+#     "${test_builtins[@]}" --release
+#     "${test_builtins[@]}" --features c
+#     "${test_builtins[@]}" --features c --release
+#     "${test_builtins[@]}" --features no-asm
+#     "${test_builtins[@]}" --features no-asm --release
+#     "${test_builtins[@]}" --features no-f16-f128
+#     "${test_builtins[@]}" --features no-f16-f128 --release
+#     "${test_builtins[@]}" --benches
+#     "${test_builtins[@]}" --benches --release
 
-    if [ "${TEST_VERBATIM:-}" = "1" ]; then
-        verb_path=$(cmd.exe //C echo \\\\?\\%cd%\\builtins-test\\target2)
-        "${test_builtins[@]}" --target-dir "$verb_path" --features c
-    fi
-fi
+#     if [ "${TEST_VERBATIM:-}" = "1" ]; then
+#         verb_path=$(cmd.exe //C echo \\\\?\\%cd%\\builtins-test\\target2)
+#         "${test_builtins[@]}" --target-dir "$verb_path" --features c
+#     fi
+# fi
 
 
-declare -a rlib_paths
+# declare -a rlib_paths
 
-# Set the `rlib_paths` global array to a list of all compiler-builtins rlibs
-update_rlib_paths() {
-    if [ -d /builtins-target ]; then
-        rlib_paths=( /builtins-target/"${target}"/debug/deps/libcompiler_builtins-*.rlib )
-    else
-        rlib_paths=( target/"${target}"/debug/deps/libcompiler_builtins-*.rlib )
-    fi
-}
+# # Set the `rlib_paths` global array to a list of all compiler-builtins rlibs
+# update_rlib_paths() {
+#     if [ -d /builtins-target ]; then
+#         rlib_paths=( /builtins-target/"${target}"/debug/deps/libcompiler_builtins-*.rlib )
+#     else
+#         rlib_paths=( target/"${target}"/debug/deps/libcompiler_builtins-*.rlib )
+#     fi
+# }
 
-# Remove any existing artifacts from previous tests that don't set #![compiler_builtins]
-update_rlib_paths
-rm -f "${rlib_paths[@]}"
+# # Remove any existing artifacts from previous tests that don't set #![compiler_builtins]
+# update_rlib_paths
+# rm -f "${rlib_paths[@]}"
 
-cargo build -p compiler_builtins --target "$target"
-cargo build -p compiler_builtins --target "$target" --release
-cargo build -p compiler_builtins --target "$target" --features c
-cargo build -p compiler_builtins --target "$target" --features c --release
-cargo build -p compiler_builtins --target "$target" --features no-asm
-cargo build -p compiler_builtins --target "$target" --features no-asm --release
-cargo build -p compiler_builtins --target "$target" --features no-f16-f128
-cargo build -p compiler_builtins --target "$target" --features no-f16-f128 --release
+# cargo build -p compiler_builtins --target "$target"
+# cargo build -p compiler_builtins --target "$target" --release
+# cargo build -p compiler_builtins --target "$target" --features c
+# cargo build -p compiler_builtins --target "$target" --features c --release
+# cargo build -p compiler_builtins --target "$target" --features no-asm
+# cargo build -p compiler_builtins --target "$target" --features no-asm --release
+# cargo build -p compiler_builtins --target "$target" --features no-f16-f128
+# cargo build -p compiler_builtins --target "$target" --features no-f16-f128 --release
 
-PREFIX=${target//unknown-/}-
-case "$target" in
-    armv7-*)
-        PREFIX=arm-linux-gnueabihf-
-        ;;
-    thumb*)
-        PREFIX=arm-none-eabi-
-        ;;
-    *86*-*)
-        PREFIX=
-        ;;
-esac
+# PREFIX=${target//unknown-/}-
+# case "$target" in
+#     armv7-*)
+#         PREFIX=arm-linux-gnueabihf-
+#         ;;
+#     thumb*)
+#         PREFIX=arm-none-eabi-
+#         ;;
+#     *86*-*)
+#         PREFIX=
+#         ;;
+# esac
 
-NM=$(find "$(rustc --print sysroot)" \( -name llvm-nm -o -name llvm-nm.exe \) )
-if [ "$NM" = "" ]; then
-  NM="${PREFIX}nm"
-fi
+# NM=$(find "$(rustc --print sysroot)" \( -name llvm-nm -o -name llvm-nm.exe \) )
+# if [ "$NM" = "" ]; then
+#   NM="${PREFIX}nm"
+# fi
 
-# i686-pc-windows-gnu tools have a dependency on some DLLs, so run it with
-# rustup run to ensure that those are in PATH.
-TOOLCHAIN="$(rustup show active-toolchain | sed 's/ (default)//')"
-if [[ "$TOOLCHAIN" == *i686-pc-windows-gnu ]]; then
-  NM="rustup run $TOOLCHAIN $NM"
-fi
+# # i686-pc-windows-gnu tools have a dependency on some DLLs, so run it with
+# # rustup run to ensure that those are in PATH.
+# TOOLCHAIN="$(rustup show active-toolchain | sed 's/ (default)//')"
+# if [[ "$TOOLCHAIN" == *i686-pc-windows-gnu ]]; then
+#   NM="rustup run $TOOLCHAIN $NM"
+# fi
 
-# Look out for duplicated symbols when we include the compiler-rt (C) implementation
-update_rlib_paths
-for rlib in "${rlib_paths[@]}"; do
-    set +x
-    echo "================================================================"
-    echo "checking $rlib for duplicate symbols"
-    echo "================================================================"
-    set -x
+# # Look out for duplicated symbols when we include the compiler-rt (C) implementation
+# update_rlib_paths
+# for rlib in "${rlib_paths[@]}"; do
+#     set +x
+#     echo "================================================================"
+#     echo "checking $rlib for duplicate symbols"
+#     echo "================================================================"
+#     set -x
     
-    duplicates_found=0
+#     duplicates_found=0
 
-    # NOTE On i586, It's normal that the get_pc_thunk symbol appears several
-    # times so ignore it
-    $NM -g --defined-only "$rlib" 2>&1 |
-      sort |
-      uniq -d |
-      grep -v __x86.get_pc_thunk --quiet |
-      grep 'T __' && duplicates_found=1
+#     # NOTE On i586, It's normal that the get_pc_thunk symbol appears several
+#     # times so ignore it
+#     $NM -g --defined-only "$rlib" 2>&1 |
+#       sort |
+#       uniq -d |
+#       grep -v __x86.get_pc_thunk --quiet |
+#       grep 'T __' && duplicates_found=1
 
-    if [ "$duplicates_found" != 0 ]; then
-        echo "error: found duplicate symbols"
-        exit 1
-    else
-        echo "success; no duplicate symbols found"
-    fi
-done
+#     if [ "$duplicates_found" != 0 ]; then
+#         echo "error: found duplicate symbols"
+#         exit 1
+#     else
+#         echo "success; no duplicate symbols found"
+#     fi
+# done
 
-rm -f "${rlib_paths[@]}"
+# rm -f "${rlib_paths[@]}"
 
-build_intrinsics_test() {
-    cargo build \
-        --target "$target" --verbose \
-        --manifest-path builtins-test-intrinsics/Cargo.toml "$@"
-}
+# build_intrinsics_test() {
+#     cargo build \
+#         --target "$target" --verbose \
+#         --manifest-path builtins-test-intrinsics/Cargo.toml "$@"
+# }
 
-# Verify that we haven't dropped any intrinsics/symbols
-build_intrinsics_test
-build_intrinsics_test --release
-build_intrinsics_test --features c
-build_intrinsics_test --features c --release
+# # Verify that we haven't dropped any intrinsics/symbols
+# build_intrinsics_test
+# build_intrinsics_test --release
+# build_intrinsics_test --features c
+# build_intrinsics_test --features c --release
 
-# Verify that there are no undefined symbols to `panic` within our
-# implementations
-CARGO_PROFILE_DEV_LTO=true build_intrinsics_test
-CARGO_PROFILE_RELEASE_LTO=true build_intrinsics_test --release
+# # Verify that there are no undefined symbols to `panic` within our
+# # implementations
+# CARGO_PROFILE_DEV_LTO=true build_intrinsics_test
+# CARGO_PROFILE_RELEASE_LTO=true build_intrinsics_test --release
 
-# Ensure no references to any symbols from core
-update_rlib_paths
-for rlib in "${rlib_paths[@]}"; do
-    set +x
-    echo "================================================================"
-    echo "checking $rlib for references to core"
-    echo "================================================================"
-    set -x
+# # Ensure no references to any symbols from core
+# update_rlib_paths
+# for rlib in "${rlib_paths[@]}"; do
+#     set +x
+#     echo "================================================================"
+#     echo "checking $rlib for references to core"
+#     echo "================================================================"
+#     set -x
 
-    tmpdir="${CARGO_TARGET_DIR:-target}/tmp"
-    test -d "$tmpdir" || mkdir "$tmpdir"
-    defined="$tmpdir/defined_symbols.txt"
-    undefined="$tmpdir/defined_symbols.txt"
+#     tmpdir="${CARGO_TARGET_DIR:-target}/tmp"
+#     test -d "$tmpdir" || mkdir "$tmpdir"
+#     defined="$tmpdir/defined_symbols.txt"
+#     undefined="$tmpdir/defined_symbols.txt"
 
-    $NM --quiet -U "$rlib" | grep 'T _ZN4core' | awk '{print $3}' | sort | uniq > "$defined"
-    $NM --quiet -u "$rlib" | grep 'U _ZN4core' | awk '{print $2}' | sort | uniq > "$undefined"
-    grep_has_results=0
-    grep -v -F -x -f "$defined" "$undefined" && grep_has_results=1
+#     $NM --quiet -U "$rlib" | grep 'T _ZN4core' | awk '{print $3}' | sort | uniq > "$defined"
+#     $NM --quiet -u "$rlib" | grep 'U _ZN4core' | awk '{print $2}' | sort | uniq > "$undefined"
+#     grep_has_results=0
+#     grep -v -F -x -f "$defined" "$undefined" && grep_has_results=1
 
-    if [ "$target" = "powerpc64-unknown-linux-gnu" ]; then
-        echo "FIXME: powerpc64 fails these tests"
-    elif [ "$grep_has_results" != 0 ]; then
-        echo "error: found unexpected references to core"
-        exit 1
-    else
-        echo "success; no references to core found"
-    fi
-done
+#     if [ "$target" = "powerpc64-unknown-linux-gnu" ]; then
+#         echo "FIXME: powerpc64 fails these tests"
+#     elif [ "$grep_has_results" != 0 ]; then
+#         echo "error: found unexpected references to core"
+#         exit 1
+#     else
+#         echo "success; no references to core found"
+#     fi
+# done
 
 # Test libm
 
