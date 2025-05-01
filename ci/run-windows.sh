3e349371fc37b9aa9cb8780fa299406be68630e8
Trevor Gross <tmgross@umich.edu>
2025-05-01 23:44:24 +0000

    inspect path

--- ci/run-windows.sh
+++ ci/run-windows.sh
@@ -7,14 +7,11 @@ target="$1"
 m4 --help
 make --help
 
+echo "$PATH"
+
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- \
     --default-toolchain nightly-x86_64-gnu \
-    --target "$target" -y
+    --target "$target" \
+    --profile minimal -y
 
-# cargo -vV
-
-# rustup update nightly-x86_64-gnu
-# rustup default nightly-x86_64-gnu
-# rustup target add "$target"
-
-./ci/run.sh "$target"
+echo "$PATH"
