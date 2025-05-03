66c8caeb04c8aecf81544d8c3bbadae9c17ce191
Mateusz Mikuła <oss@mateuszmikula.dev>
2025-05-03 18:44:55 +0200

    bash_profile?

--- ci/run-windows.sh
+++ ci/run-windows.sh
@@ -15,6 +15,6 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- \
     --profile minimal -y
 
 ls ~/
-echo 'export PATH="/c/Users/$USERNAME/.cargo/bin:$PATH"' >> ~/.profile
+echo 'export PATH="/c/Users/$USERNAME/.cargo/bin:$PATH"' >> ~/.bash_profile
 
 echo "$PATH"
