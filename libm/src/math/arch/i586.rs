8f43b0b5f568c688c3504f89097221c2d16749b4
Folkert de Vries <folkert@folkertdev.nl>
2025-07-17 12:24:32 +0200

    implement `floor` and `ceil` with inline assembly on `i586`

--- libm/src/math/arch/i586.rs
+++ libm/src/math/arch/i586.rs
@@ -6,31 +6,33 @@
 ///
 /// Based on https://github.com/NetBSD/src/blob/trunk/lib/libm/arch/i387/s_ceil.S
 /// (written by J.T. Conklin <jtc@NetBSD.org>).
-#[unsafe(naked)]
-pub extern "C" fn ceil(_: f64) -> f64 {
-    core::arch::naked_asm!(
-        "pushl  %ebp",
-        "movl   %esp,%ebp",
-        "subl   $8,%esp",
-        // Store fpu control word.
-        "fstcw   -4(%ebp)",
-        "movw    -4(%ebp),%dx",
-        // Round towards +oo.
-        "orw $0x0800,%dx",
-        "andw    $0xfbff,%dx",
-        "movw    %dx,-8(%ebp)",
-        // Load modified control word
-        "fldcw   -8(%ebp)",
-        // Round.
-        "fldl    8(%ebp)",
-        "frndint",
-        // Restore original control word.
-        "fldcw   -4(%ebp)",
-        // Restore esp and ebp and return
-        "leave",
-        "ret",
-        options(att_syntax)
-    )
+pub fn ceil(mut x: f64) -> f64 {
+    // We save and later restore the FPU control word.
+    let mut cw_stash = core::mem::MaybeUninit::<u16>::uninit();
+    let mut cw_tmp = core::mem::MaybeUninit::<u16>::uninit();
+    unsafe {
+        core::arch::asm!(
+            "fstcw   ({stash_ptr})",      // Save the cw
+            "movw    ({stash_ptr}), %dx", // ...
+            "orw     $0x0800, %dx",    // Set rounding control to 0b10 (+∞),
+            "andw    $0xfbff, %dx",    // preserving other controls
+            "movw    %dx, ({cw_ptr})", // Apply cw
+            "fldcw    ({cw_ptr})",     // ...
+            "fldl     ({x_ptr})",      // Push x to the stack
+            "frndint",                 // Round
+            "fldcw    ({stash_ptr})",  // Restore cw
+            "fstpl   ({x_ptr})",       // Save rounded x to mem
+            cw_ptr = in(reg) &mut cw_tmp,
+            stash_ptr = in(reg) &mut cw_stash,
+            x_ptr = in(reg) &mut x,
+            out("dx") _,               // Cw scratch
+            // All the x87 FPU stack is used, all registers must be clobbered
+            out("st(0)") _, out("st(1)") _, out("st(2)") _, out("st(3)") _,
+            out("st(4)") _, out("st(5)") _, out("st(6)") _, out("st(7)") _,
+            options(att_syntax)
+        )
+    }
+    x
 }
 
 /// Use an alternative implementation on x86, because the
@@ -39,29 +41,31 @@ pub extern "C" fn ceil(_: f64) -> f64 {
 ///
 /// Based on https://github.com/NetBSD/src/blob/trunk/lib/libm/arch/i387/s_floor.S
 /// (written by J.T. Conklin <jtc@NetBSD.org>).
-#[unsafe(naked)]
-pub extern "C" fn floor(_: f64) -> f64 {
-    core::arch::naked_asm!(
-        "pushl  %ebp",
-        "movl   %esp,%ebp",
-        "subl   $8,%esp",
-        // Store fpu control word.
-        "fstcw   -4(%ebp)",
-        "movw    -4(%ebp),%dx",
-        // Round towards -oo.
-        "orw	$0x0400,%dx",
-        "andw	$0xf7ff,%dx",
-        "movw   %dx,-8(%ebp)",
-        // Load modified control word
-        "fldcw   -8(%ebp)",
-        // Round.
-        "fldl    8(%ebp)",
-        "frndint",
-        // Restore original control word.
-        "fldcw   -4(%ebp)",
-        // Restore esp and ebp and return
-        "leave",
-        "ret",
-        options(att_syntax)
-    )
+pub fn floor(mut x: f64) -> f64 {
+    // We save and later restore the FPU control word.
+    let mut cw_stash = core::mem::MaybeUninit::<u16>::uninit();
+    let mut cw_tmp = core::mem::MaybeUninit::<u16>::uninit();
+    unsafe {
+        core::arch::asm!(
+            "fstcw   ({stash_ptr})",      // Save the cw
+            "movw    ({stash_ptr}), %dx", // ...
+            "orw     $0x0400, %dx",    // Set rounding control to 0b01 (-∞),
+            "andw    $0xf7ff, %dx",    // preserving other controls
+            "movw    %dx, ({cw_ptr})", // Apply cw
+            "fldcw    ({cw_ptr})",     // ...
+            "fldl     ({x_ptr})",      // Push x to the stack
+            "frndint",                 // Round
+            "fldcw    ({stash_ptr})",  // Restore cw
+            "fstpl   ({x_ptr})",       // Save rounded x to mem
+            cw_ptr = in(reg) &mut cw_tmp,
+            stash_ptr = in(reg) &mut cw_stash,
+            x_ptr = in(reg) &mut x,
+            out("dx") _,               // Cw scratch
+            // All the x87 FPU stack is used, all registers must be clobbered
+            out("st(0)") _, out("st(1)") _, out("st(2)") _, out("st(3)") _,
+            out("st(4)") _, out("st(5)") _, out("st(6)") _, out("st(7)") _,
+            options(att_syntax)
+        )
+    }
+    x
 }
