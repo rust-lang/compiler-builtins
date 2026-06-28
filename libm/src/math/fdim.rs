3fdf4ae640fc9fc845457ed0c5bafc81792dfb16
dotacow <ykitaneh22@gmail.com>
2026-06-28 15:56:24 +0300

    chore: apply rustfmt

--- libm/src/math/fdim.rs
+++ libm/src/math/fdim.rs
@@ -2,8 +2,8 @@
 ///
 /// Determines the positive difference between arguments, returning:
 /// * x - y if x > y, or
-/// * +0    if x <= y, or
-/// * NAN   if either argument is NAN.
+/// * +0 if x <= y, or
+/// * NAN if either argument is NAN.
 ///
 /// A range error may occur.
 #[cfg(f16_enabled)]
@@ -16,8 +16,8 @@ pub fn fdimf16(x: f16, y: f16) -> f16 {
 ///
 /// Determines the positive difference between arguments, returning:
 /// * x - y if x > y, or
-/// * +0    if x <= y, or
-/// * NAN   if either argument is NAN.
+/// * +0 if x <= y, or
+/// * NAN if either argument is NAN.
 ///
 /// A range error may occur.
 #[cfg_attr(assert_no_panic, no_panic::no_panic)]
@@ -29,8 +29,8 @@ pub fn fdimf(x: f32, y: f32) -> f32 {
 ///
 /// Determines the positive difference between arguments, returning:
 /// * x - y if x > y, or
-/// * +0    if x <= y, or
-/// * NAN   if either argument is NAN.
+/// * +0 if x <= y, or
+/// * NAN if either argument is NAN.
 ///
 /// A range error may occur.
 #[cfg_attr(assert_no_panic, no_panic::no_panic)]
@@ -42,8 +42,8 @@ pub fn fdim(x: f64, y: f64) -> f64 {
 ///
 /// Determines the positive difference between arguments, returning:
 /// * x - y if x > y, or
-/// * +0    if x <= y, or
-/// * NAN   if either argument is NAN.
+/// * +0 if x <= y, or
+/// * NAN if either argument is NAN.
 ///
 /// A range error may occur.
 #[cfg(f128_enabled)]
