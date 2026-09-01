rust-lang/compiler-builtins

commit 11eda6a4d5a02ef2a6dfe46b00ab32cafe15cdcf

c-b: Remove `#[aapcs_on_arm]`
Fixes rust-lang/compiler-builtins#1271 by removing `#[aapcs_on_arm]`:
`compiler-rt` only does the equivalent on ARM soft-float targets where
the `"C"` ABI is already AAPCS.

[ add PR description to commit - Trevor ]

compiler-builtins/src/float/extend.rs
@@ -69,7 +69,6 @@ where
 }
 
 intrinsics! {
-    #[aapcs_on_arm]
     #[arm_aeabi_alias = __aeabi_f2d]
     pub extern "C" fn  __extendsfdf2(a: f32) -> f64 {
         extend(a)
@@ -77,7 +76,6 @@ intrinsics! {
 }
 
 intrinsics! {
-    #[aapcs_on_arm]
     #[apple_f16_arg_abi]
     #[arm_aeabi_alias = __aeabi_h2f]
     #[cfg(f16_enabled)]
@@ -85,35 +83,30 @@ intrinsics! {
         extend(a)
     }
 
-    #[aapcs_on_arm]
     #[apple_f16_arg_abi]
     #[cfg(f16_enabled)]
     pub extern "C" fn __gnu_h2f_ieee(a: f16) -> f32 {
         extend(a)
     }
 
-    #[aapcs_on_arm]
     #[apple_f16_arg_abi]
     #[cfg(f16_enabled)]
     pub extern "C" fn __extendhfdf2(a: f16) -> f64 {
         extend(a)
     }
 
-    #[aapcs_on_arm]
     #[ppc_name = __extendhfkf2]
     #[cfg(all(f16_enabled, f128_enabled))]
     pub extern "C" fn __extendhftf2(a: f16) -> f128 {
         extend(a)
     }
 
-    #[aapcs_on_arm]
     #[ppc_name = __extendsfkf2]
     #[cfg(f128_enabled)]
     pub extern "C" fn __extendsftf2(a: f32) -> f128 {
         extend(a)
     }
 
-    #[aapcs_on_arm]
     #[ppc_name = __extenddfkf2]
     #[cfg(f128_enabled)]
     pub extern "C" fn __extenddftf2(a: f64) -> f128 {

compiler-builtins/src/float/trunc.rs
@@ -114,7 +114,6 @@ where
 }
 
 intrinsics! {
-    #[aapcs_on_arm]
     #[arm_aeabi_alias = __aeabi_d2f]
     pub extern "C" fn __truncdfsf2(a: f64) -> f32 {
         trunc(a)
@@ -122,7 +121,6 @@ intrinsics! {
 }
 
 intrinsics! {
-    #[aapcs_on_arm]
     #[apple_f16_ret_abi]
     #[arm_aeabi_alias = __aeabi_f2h]
     #[cfg(f16_enabled)]
@@ -130,14 +128,12 @@ intrinsics! {
         trunc(a)
     }
 
-    #[aapcs_on_arm]
     #[apple_f16_ret_abi]
     #[cfg(f16_enabled)]
     pub extern "C" fn __gnu_f2h_ieee(a: f32) -> f16 {
         trunc(a)
     }
 
-    #[aapcs_on_arm]
     #[apple_f16_ret_abi]
     #[arm_aeabi_alias = __aeabi_d2h]
     #[cfg(f16_enabled)]
@@ -145,21 +141,18 @@ intrinsics! {
         trunc(a)
     }
 
-    #[aapcs_on_arm]
     #[ppc_name = __trunckfhf2]
     #[cfg(all(f16_enabled, f128_enabled))]
     pub extern "C" fn __trunctfhf2(a: f128) -> f16 {
         trunc(a)
     }
 
-    #[aapcs_on_arm]
     #[ppc_name = __trunckfsf2]
     #[cfg(f128_enabled)]
     pub extern "C" fn __trunctfsf2(a: f128) -> f32 {
         trunc(a)
     }
 
-    #[aapcs_on_arm]
     #[ppc_name = __trunckfdf2]
     #[cfg(f128_enabled)]
     pub extern "C" fn __trunctfdf2(a: f128) -> f64 {

compiler-builtins/src/float/add.rs
@@ -195,13 +195,11 @@ intrinsics! {
         add(a, b)
     }
 
-    #[aapcs_on_arm]
     #[arm_aeabi_alias = __aeabi_fadd]
     pub extern "C" fn __addsf3(a: f32, b: f32) -> f32 {
         add(a, b)
     }
 
-    #[aapcs_on_arm]
     #[arm_aeabi_alias = __aeabi_dadd]
     pub extern "C" fn __adddf3(a: f64, b: f64) -> f64 {
         add(a, b)

compiler-builtins/src/float/mul.rs
@@ -184,13 +184,11 @@ intrinsics! {
         mul(a, b)
     }
 
-    #[aapcs_on_arm]
     #[arm_aeabi_alias = __aeabi_fmul]
     pub extern "C" fn __mulsf3(a: f32, b: f32) -> f32 {
         mul(a, b)
     }
 
-    #[aapcs_on_arm]
     #[arm_aeabi_alias = __aeabi_dmul]
     pub extern "C" fn __muldf3(a: f64, b: f64) -> f64 {
         mul(a, b)

compiler-builtins/src/macros.rs
@@ -38,12 +38,9 @@
 ///
 /// A quick overview of attributes supported right now are:
 ///
+// FIXME: Add missing attributes.
 /// * `maybe_use_optimized_c_shim` - indicates that the Rust implementation is
 ///   ignored if an optimized C version was compiled.
-/// * `aapcs_on_arm` - forces the ABI of the function to be `"aapcs"` on ARM and
-///   the specified ABI everywhere else.
-/// * `unadjusted_on_win64` - like `aapcs_on_arm` this switches to the
-///   `"unadjusted"` abi on Win64 and the specified abi elsewhere.
 /// * `arm_aeabi_alias` - handles the "aliasing" of various intrinsics on ARM
 ///   their otherwise typical names to other prefixed ones.
 /// * `ppc_name` - changes the name of the symbol on PowerPC platforms without
@@ -170,38 +167,7 @@ macro_rules! intrinsics {
         intrinsics!($($rest)*);
     );
 
-    // We recognize the `#[aapcs_on_arm]` attribute here and generate the
-    // same intrinsic but force it to have the `"aapcs"` calling convention on
-    // ARM and `"C"` elsewhere.
-    (
-        #[aapcs_on_arm]
-        $(#[$($attr:tt)*])*
-        pub extern $abi:tt fn $name:ident( $($argname:ident:  $ty:ty),* ) $(-> $ret:ty)? {
-            $($body:tt)*
-        }
-
-        $($rest:tt)*
-    ) => (
-        #[cfg(target_arch = "arm")]
-        intrinsics! {
-            $(#[$($attr)*])*
-            pub extern "aapcs" fn $name( $($argname: $ty),* ) $(-> $ret)? {
-                $($body)*
-            }
-        }
-
-        #[cfg(not(target_arch = "arm"))]
-        intrinsics! {
-            $(#[$($attr)*])*
-            pub extern $abi fn $name( $($argname: $ty),* ) $(-> $ret)? {
-                $($body)*
-            }
-        }
-
-        intrinsics!($($rest)*);
-    );
-
-    // `arm_aeabi_alias` would conflict with `f16_apple_{arg,ret}_abi` not handled here. Avoid macro ambiguity by combining in a
+    // `arm_aeabi_alias` would conflict with `apple_f16_{arg,ret}_abi` not handled here. Avoid macro ambiguity by combining in a
     // single `#[]`.
     (
         #[apple_f16_arg_abi]

commit b352815b5040b8588133c447d0358803a9c3b74d

c-b: Use integers in `__gnu_f2h_ieee` and `__gnu_h2f_ieee` signatures
GCC's definition of these functions uses integers in the signature
rather than `_Float16` and `float`, while compiler-rt uses AAPCS (via
`AEABI_RTABI`). These happen to match up because floats and integers are
passed the same with non-vector AAPCS. No other mismatches showed up
because LLVM emits these versions only on 32-bit arm, and should be
keeping that behavior.

When we had `aapcs_on_arm` our definitions matched up as well, but this
was removed recently. Change our signatures to match the GCC definition.
`apple_f16_*_abi` is no longer needed as a result, though this libcall
was not used on Apple platforms anyway.

Fixes: 11eda6a4d5a0 ("c-b: Remove `#[aapcs_on_arm]`")

[1]: https://github.com/llvm/llvm-project/blob/cd08ff07cee9e29bca1c5db229b1cf43d3f2c572/compiler-rt/lib/builtins/extendhfsf2.c#L19-L29

compiler-builtins/src/float/extend.rs
@@ -83,10 +83,9 @@ intrinsics! {
         extend(a)
     }
 
-    #[apple_f16_arg_abi]
     #[cfg(f16_enabled)]
-    pub extern "C" fn __gnu_h2f_ieee(a: f16) -> f32 {
-        extend(a)
+    pub extern "C" fn __gnu_h2f_ieee(a: u16) -> u32 {
+        extend::<f16, f32>(f16::from_bits(a)).to_bits()
     }
 
     #[apple_f16_arg_abi]

compiler-builtins/src/float/trunc.rs
@@ -128,10 +128,9 @@ intrinsics! {
         trunc(a)
     }
 
-    #[apple_f16_ret_abi]
     #[cfg(f16_enabled)]
-    pub extern "C" fn __gnu_f2h_ieee(a: f32) -> f16 {
-        trunc(a)
+    pub extern "C" fn __gnu_f2h_ieee(a: u32) -> u16 {
+        trunc::<f32, f16>(f32::from_bits(a)).to_bits()
     }
 
     #[apple_f16_ret_abi]

builtins-test/tests/conv.rs
@@ -314,6 +314,13 @@ mod extend {
         pub use compiler_builtins::float::extend::__extendsfdf2;
         #[cfg(f16_enabled)]
         pub use compiler_builtins::float::extend::{__extendhfdf2, __extendhfsf2, __gnu_h2f_ieee};
+
+        #[cfg(f16_enabled)]
+        pub fn check_gnu_h2f_ieee(x: f16) -> f32 {
+            f32::from_bits(compiler_builtins::float::extend::__gnu_h2f_ieee(
+                x.to_bits(),
+            ))
+        }
     }
 
     use super::*;
@@ -322,7 +329,7 @@ mod extend {
     f_to_f! {
         extend,
         f16 => f32, Half => Single, __extendhfsf2, not(no_sys_f16);
-        f16 => f32, Half => Single, __gnu_h2f_ieee, not(no_sys_f16);
+        f16 => f32, Half => Single, check_gnu_h2f_ieee, not(no_sys_f16);
         f16 => f64, Half => Double, __extendhfdf2, not(no_sys_f16_f64_convert);
     }
 
@@ -366,6 +373,11 @@ mod trunc {
         pub use compiler_builtins::float::trunc::__truncdfsf2;
         #[cfg(f16_enabled)]
         pub use compiler_builtins::float::trunc::{__gnu_f2h_ieee, __truncdfhf2, __truncsfhf2};
+
+        #[cfg(f16_enabled)]
+        pub fn check_gnu_f2h_ieee(x: f32) -> f16 {
+            f16::from_bits(compiler_builtins::float::trunc::__gnu_f2h_ieee(x.to_bits()))
+        }
     }
 
     use super::*;
@@ -374,7 +386,7 @@ mod trunc {
     f_to_f! {
         trunc,
         f32 => f16, Single => Half, __truncsfhf2, not(no_sys_f16);
-        f32 => f16, Single => Half, __gnu_f2h_ieee, not(no_sys_f16);
+        f32 => f16, Single => Half, check_gnu_f2h_ieee, not(no_sys_f16);
         f64 => f16, Double => Half, __truncdfhf2, not(no_sys_f16_f64_convert);
     }
 